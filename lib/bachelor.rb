3d1fc17f631919c8464e37bc4677a3e7fd0cb270
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-21 01:23:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,7 @@ def get_contestant_name(data, occupation)
   # code here
   data.each do |season, array|
     array.each do |people|
+      people.each do |
 end
 
 def count_contestants_by_hometown(data, hometown)
